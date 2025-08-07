4619590ab9ceaa1b379b019fcef57ddb6e0d62f7
Amrita <amyshahi538@gmail.com>
2025-08-07 19:29:08 +0545

    Add requirements.txt for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dd_ko_maya.wsgi
---
+++ main/context_processors.py
@@ -0,0 +1,6 @@
+def bank_details(request):
+    return {
+        'bank_name': 'NIC ASIA BANK',
+        'account_name': 'Amrita Shahi',
+        'account_number': '0545752244974001'
+    }
